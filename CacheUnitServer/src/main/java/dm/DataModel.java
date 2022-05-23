20d1f576ab822e5a3aa18360b95dbcf2386f3846
IzabelaB205 <izabelablitsman@gmail.com>
2022-05-23 20:23:05 +0300

    represent the data model for server and client communication

---
+++ CacheUnitServer/src/main/java/dm/DataModel.java
@@ -0,0 +1,30 @@
+package main.java.dm;
+
+import java.io.Serializable;
+
+public class DataModel<T> implements Serializable {
+    private long id;
+    private T content;
+
+    public DataModel(long id, T content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() { return id; }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public T getContent() { return content; }
+
+    @Override
+    public String toString() {
+        return "id:  " + id + " name:  " + content.toString();
+    }
+}
