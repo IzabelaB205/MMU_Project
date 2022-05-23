f0f1a6706163ab0df2288f9e78e4efd0a7cd8bd8
IzabelaB205 <izabelablitsman@gmail.com>
2022-05-23 21:24:24 +0300

    adding an interface for file improved actions

---
+++ CacheUnitServer/src/main/java/dao/IDao.java
@@ -0,0 +1,10 @@
+package main.java.dao;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public interface IDao <ID extends Serializable, T> {
+    T find(ID id) throws IOException, ClassNotFoundException;
+    void save(T value);
+    void remove(T value);
+}
