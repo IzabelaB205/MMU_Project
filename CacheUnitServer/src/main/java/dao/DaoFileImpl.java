77a76c5e510593ab935f06f88f1bdde71d283e8d
IzabelaB205 <izabelablitsman@gmail.com>
2022-05-23 22:02:15 +0300

    implement the logic of IDao interface - read, save and remove data model from the file given by the user

---
+++ CacheUnitServer/src/main/java/dao/DaoFileImpl.java
@@ -0,0 +1,67 @@
+package main.java.dao;
+
+import main.java.dm.DataModel;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DaoFileImpl<T> implements IDao<Long, DataModel<T>> {
+    private final String filePath;
+    private Map<Long, DataModel<T>> fileObjectsMap;
+
+    public DaoFileImpl(String filePath) {
+        this.filePath = filePath;
+        fileObjectsMap = new HashMap<>();
+    }
+
+
+    @Override
+    public DataModel<T> find(Long key) throws IOException, ClassNotFoundException {
+        DataModel<T> dataModel = null;
+        readFileObjects();
+
+        if(fileObjectsMap.containsKey(key)) {
+            dataModel = fileObjectsMap.get(key);
+        }
+
+        return dataModel;
+    }
+
+    @Override
+    public void save(DataModel<T> dataModel) {
+        fileObjectsMap.put(dataModel.getId(), dataModel);
+
+        try(ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            output.writeObject(fileObjectsMap);
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void remove(DataModel<T> dataModel) {
+        fileObjectsMap.remove(dataModel.getId());
+
+        try(ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            output.writeObject(fileObjectsMap);
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readFileObjects() {
+        try(ObjectInputStream input = new ObjectInputStream(new FileInputStream(filePath))) {
+            fileObjectsMap = (Map<Long, DataModel<T>>) input.readObject();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
