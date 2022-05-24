aee356eb8c3991eafb59fc6c98c3565040d8a69d
IzabelaB205 <izabelablitsman@gmail.com>
2022-05-24 17:48:29 +0300

    adding server request message structure that includes the header type and the body

---
+++ CacheUnitServer/src/main/java/server/Request.java
@@ -0,0 +1,36 @@
+package server;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class Request<T> implements Serializable {
+    private Map<String, String> header;
+    private T body;
+
+    public Request(Map<String, String> header, T body) {
+        this.header = header;
+        this.body = body;
+    }
+
+    public T getBody() {
+        return body;
+    }
+
+    public void setBody(T body) {
+        this.body = body;
+    }
+
+    public Map<String, String> getHeader() {
+        return header;
+    }
+
+    public void setHeader(Map<String, String> header) {
+        this.header = header;
+    }
+
+    @Override
+    public String toString() {
+        //maybe should to do body.toString() - check in test
+        return super.toString();
+    }
+}
