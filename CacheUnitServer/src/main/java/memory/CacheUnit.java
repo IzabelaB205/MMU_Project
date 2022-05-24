63eec4d3d7a3eb904a231b42a4b9e2439cb7a88a
IzabelaB205 <izabelablitsman@gmail.com>
2022-05-24 13:24:29 +0300

    implement the logic of cache unit by using strategy design pattern for selecting cache algorithm at runtime.

---
+++ CacheUnitServer/src/main/java/memory/CacheUnit.java
@@ -0,0 +1,62 @@
+package main.java.memory;
+
+import main.java.Algorithm.ICacheAlgorithm;
+import main.java.dao.IDao;
+import main.java.dm.DataModel;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CacheUnit<T> {
+    private ICacheAlgorithm<Long, DataModel<T>> cacheAlgorithm;
+    private IDao<Serializable, DataModel<T>> dao;
+
+    public CacheUnit(ICacheAlgorithm<Long, DataModel<T>> cacheAlgorithm, IDao<Serializable, DataModel<T>> dao) {
+        this.cacheAlgorithm = cacheAlgorithm;
+        this.dao = dao;
+    }
+
+    public DataModel<T>[] getDataModels(Long[] ids) {
+        List<DataModel<T>> result = new ArrayList<>();
+
+        try {
+            for(long id : ids) {
+                DataModel<T> value = cacheAlgorithm.getElement(id);
+
+                // The page did not exist in the cache memory.
+                // Find the relevant page in the hard drive.
+                if(value == null) {
+                    value = dao.find(id);
+                    cacheAlgorithm.putElement(id, value);
+                }
+
+                result.add(value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return (DataModel<T>[]) result.toArray();
+    }
+
+    public DataModel<T>[] putDataModels(DataModel<T>[] dataModels) {
+        List<DataModel<T>> dataModelList = new ArrayList<>();
+
+        for(DataModel<T> dataModel : dataModels) {
+            long key = dataModel.getId();
+            DataModel<T> result = cacheAlgorithm.putElement(key, dataModel);
+
+            if(result != null) {
+                dataModelList.add(result);
+            }
+        }
+
+        return (DataModel<T>[]) dataModelList.toArray();
+    }
+
+
+}
